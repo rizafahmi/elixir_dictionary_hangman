947b14c70e69bd91e453858200263c49e673b514
Riza Fahmi <rizafahmi@gmail.com>
2017-11-26 21:22:58 +0700

    Refactor to pipe operator

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 
   def word_list do
-    contents = File.read!("assets/words.txt")
-    String.split(contents, "\n")
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split("\n")
   end
 end
